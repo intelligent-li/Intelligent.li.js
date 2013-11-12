c06137ee0d7fb28d9ac20ad860dffda1ad4c0152
markjasonanderson <manderson@dius.com.au>
2013-11-13 09:21:43 +1100

    added meteor package file

---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+      summary: "Intelligent.li API Meteor Package"
+});
+
+Npm.depends({
+    "winston": "~0.7.2",
+    "jquery": "~1.8.3",
+    "wolfy87-eventemitter": "~4.2.5",
+    "heir": "~2.0.0",
+    "ws": "~0.4.31"
+});
+
+Package.on_use(function (api) {
+    api.add_files([
+        'lib/Api.js',
+        'lib/Feed.js',
+        'lib/ObservableMap.js', 
+        'lib/ResourceCache.js',
+        'lib/Device.js',
+        'lib/Logger.js',
+        'lib/Resource.js',
+        'lib/SampleStore.js'
+        ],
+        'server'); 
+
+    if (typeof api.export !== 'undefined') {
+        api.export(['my'], 'server'); 
+    }
+});
