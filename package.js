fcb33682f1b6e578ff4585a6542f72118ef7443a
markjasonanderson <manderson@dius.com.au>
2013-11-21 13:19:59 +1100

    now uses keep-alive-agent to speed things up

--- package.js
+++ package.js
@@ -3,41 +3,44 @@ Package.describe({
 });
 
 Npm.depends({
-    "winston": "0.7.2",
-    "wolfy87-eventemitter": "4.2.5",
-    "heir": "2.0.0",
-    "ws": "0.4.31"
+  "keep-alive-agent" : "0.0.1",
+  "winston": "0.7.2",
+  "wolfy87-eventemitter": "4.2.5",
+  "heir": "2.0.0",
+  "ws": "0.4.31"
 });
 
 Package.on_use(function (api) {
   api.add_files('lib/Utils.js', ['client', 'server']);
 
-    api.add_files([
-        '.npm/package/node_modules/wolfy87-eventemitter/EventEmitter.js',
-        '.npm/package/node_modules/heir/heir.js'
-        ],
-        ['client']); 
+  api.add_files([
+    '.npm/package/node_modules/wolfy87-eventemitter/EventEmitter.js',
+    '.npm/package/node_modules/heir/heir.js'
+    ],
+    ['client']); 
 
-    api.add_files([
-        'lib/Logger.js',
-        'lib/ObservableMap.js', 
-        'lib/SampleStore.js',
-        'lib/ApiBase.js'
-        ],
-        ['client', 'server']); 
+  api.add_files([
+    'lib/Logger.js',
+    'lib/ObservableMap.js', 
+    'lib/SampleStore.js',
+    'lib/ApiBase.js'
+    ],
+    ['client', 'server']); 
 
-    api.add_files('lib/Api.js', 'server');
-    api.add_files('lib/ApiMeteorClient.js', 'client');
+  api.add_files('lib/Api.js', 'server');
+  api.add_files('lib/ApiMeteorClient.js', 'client');
 
-    api.add_files([
-        'lib/Resource.js',
-        'lib/ResourceCache.js',
-        'lib/Feed.js',
-        'lib/Device.js',
-        ],
-        ['client', 'server']); 
+  api.add_files([
+    'lib/Resource.js',
+    'lib/ResourceCache.js',
+    'lib/Feed.js',
+    'lib/Device.js',
+    'lib/Collection.js',
+    'lib/Source.js',
+    ],
+    ['client', 'server']); 
 
-    api.add_files('lib/MeteorConnector.js', 'server');
+  api.add_files('lib/MeteorConnector.js', 'server');
 
-    api.export(['ili'], ['client','server']); 
+  api.export(['ili'], ['client','server']); 
 });
