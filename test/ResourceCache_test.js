eee37c6a140921dd8c319adf55f8455ce3ad8414
markjasonanderson <manderson@dius.com.au>
2014-02-12 16:50:55 +1100

    Added query implementation to use new intelligent.li search interface

--- test/ResourceCache_test.js
+++ test/ResourceCache_test.js
@@ -11,7 +11,6 @@ describe("ResourceCache", function(done) {
 
   describe("constructor", function() {
     it("should call the parent constructor", function() {
-
     });
   });
 
@@ -46,6 +45,53 @@ describe("ResourceCache", function(done) {
 
       expect(rc.resources).to.not.have.keys(['123456']);
 
+    });
+  });
+
+  describe("#queryResource", function() {
+    var stub;
+    afterEach(function(){
+      stub1 && stub1.restore();
+      stub2 && stub2.restore();
+    });
+
+    it("should perform a query on ili", function() {
+      stub1 = sinon.spy(api, 'queryResource');
+      stub2 = sinon.stub(api, 'loadResource');
+
+      var rc = new ResourceCache("test");
+      rc.create = function(id) {
+        return new Feed(id);
+      }
+      var cb = function(success, result) {}
+      rc.query("*", cb);
+
+      expect(stub1).to.be.called;
+      expect(stub1).to.be.calledWith("*", rc.type, cb);
+
+      expect(stub2).to.be.called;
+      expect(stub2).to.be.calledWith("test/search?q=*");
+    });
+
+    it("should notify with search results", function(done) {
+      stub1 = sinon.spy(api, 'queryResource');
+
+      stub2 = sinon.stub(api, 'loadResource', function(r, m, notify) {
+        m.insert("1", "11");
+        m.insert("2", "2");
+        notify(true, m);
+      });
+
+      var rc = new ResourceCache("test");
+      rc.create = function(id) {
+        return new Feed(id);
+      }
+
+      rc.query("*", function(success, result) {
+        expect(success).to.be.true;
+        expect(result.length).to.equal(2);
+        done();
+      });
     });
   });
 });
